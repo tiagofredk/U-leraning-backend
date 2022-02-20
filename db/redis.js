d1af8072035aaab34171d4bdd15f07caed960695
tiagofredk <tiagofredk@gmail.com>
2022-02-20 10:21:35 +0100

    undefined

--- db/redis.js
+++ db/redis.js
@@ -1,8 +1,17 @@
 const redis = require('redis');
 // 1 configure our redis
 const redisClient = redis.createClient({
-    port: 6379,
-    host: 'localhost'
+    // port: 6379,
+    // host: 'localhost'
+    host: 'gusc1-bursting-wallaby-31541.upstash.io',
+    port: '31541',
+    password: '853aaa5b1c9446c29ac6428710706604'
 });
+
+redisClient.on("error", function (err) {
+    throw err;
+});
+
+redisClient.set('foo', 'bar');
 
 module.exports = redisClient;
